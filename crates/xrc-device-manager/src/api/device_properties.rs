0cfee9529d23a5e8decd9ffde9203098eaf4f161
Leonid Meleshin <hello@leon0399.ru>
2024-05-06 22:46:55 +0400

    🚧(devices): add battery levels property

--- crates/xrc-device-manager/src/api/device_properties.rs
+++ crates/xrc-device-manager/src/api/device_properties.rs
@@ -1,42 +1,88 @@
 use derivative::Derivative;
+use getset::{Getters, Setters};
 
-#[cfg(any(feature = "mockall", test))]
-use mockall::{automock, predicate::*};
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Derivative, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derivative(Default)]
+pub enum DeviceBatteryStatus {
+  Charging,
+  Discharging,
+  Full,
+  NotCharging,
+  #[derivative(Default)]
+  Unknown,
+}
 
-#[cfg_attr(any(feature = "mockall", test), automock)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Derivative, Setters, Getters, Debug, Clone)]
+#[derivative(Default)]
+pub struct DeviceBatteryProperty {
+  #[getset(get = "pub", set = "pub")]
+  level: f32,
+  #[getset(get = "pub", set = "pub")]
+  status: DeviceBatteryStatus,
+  #[getset(get = "pub", set = "pub")]
+  voltage: Option<f32>,
+}
+
+impl DeviceBatteryProperty {
+  #[inline(always)]
+  pub fn new(level: f32, status: DeviceBatteryStatus, voltage: Option<f32>) -> Self {
+    Self {
+      level,
+      status,
+      voltage,
+    }
+  }
+}
+
 pub trait DeviceProperties {
-  fn hardware_version(&self) -> &String;
+  fn hardware_version(&self) -> Option<&str>;
 
-  fn firmware_version(&self) -> &String;
+  fn firmware_version(&self) -> Option<&str>;
 
-  // todo: add battery_levels
+  fn battery_levels(&self) -> Option<&Vec<DeviceBatteryProperty>> {
+    None
+  }
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Derivative, Debug, Clone)]
+#[derivative(Default)]
 pub struct GenericDeviceProperties {
-  hardware_version: String,
-  firmware_version: String,
+  hardware_version: Option<String>,
+  firmware_version: Option<String>,
+  battery_levels: Option<Vec<DeviceBatteryProperty>>,
 }
 
 impl GenericDeviceProperties {
   #[inline(always)]
-  pub fn new(hardware_version: String, firmware_version: String) -> Self {
+  pub fn new(
+    hardware_version: Option<String>,
+    firmware_version: Option<String>,
+    battery_levels: Option<Vec<DeviceBatteryProperty>>,
+  ) -> Self {
     Self {
       hardware_version,
       firmware_version,
+      battery_levels,
     }
   }
 }
 
 impl DeviceProperties for GenericDeviceProperties {
   #[inline(always)]
-  fn hardware_version(&self) -> &String {
-    &self.hardware_version
+  fn hardware_version(&self) -> Option<&str> {
+    self.hardware_version.as_deref()
   }
 
   #[inline(always)]
-  fn firmware_version(&self) -> &String {
-    &self.firmware_version
+  fn firmware_version(&self) -> Option<&str> {
+    self.firmware_version.as_deref()
+  }
+
+  #[inline(always)]
+  fn battery_levels(&self) -> Option<&Vec<DeviceBatteryProperty>> {
+    self.battery_levels.as_ref()
   }
 }
