f5dee113f679fe40ec3fb438317adbabee4ff950
Leonid Meleshin <hello@leon0399.ru>
2025-08-23 20:31:04 +0200

    ♻️ (OSC): update config structs

--- crates/ss-osc/src/server/router/config.rs
+++ crates/ss-osc/src/server/router/config.rs
@@ -1,30 +1,39 @@
 use derivative::Derivative;
 
-#[derive(Derivative, Debug, Clone, PartialEq, Eq)]
-#[derivative(Default)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
-pub enum OscRouterTargetConfigProtocol {
-    #[cfg_attr(feature = "serde", serde(rename = "udp"))]
-    OscUdp,
-
-    #[cfg_attr(feature = "serde", serde(rename = "tcp"))]
-    OscTcp,
-
-    #[derivative(Default)]
-    #[cfg_attr(feature = "serde", serde(rename = "websocket"))]
-    OscWebsocket,
+pub struct OscRouterForwardTargetOscUdpConfig {
+    to: std::net::SocketAddr,
+    from: std::net::SocketAddr,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
-pub struct OscRouterTargetConfig {
-    // todo: add transport types (e.g., TCP, WebSocket)
-
-    /// The target address to route the packet to.
+pub struct OscRouterForwardTargetOscTcpConfig {
     to: std::net::SocketAddr,
+    // from: std::net::SocketAddr,
+}
 
-    #[cfg_attr(feature = "serde", serde(default))]
-    protocol: OscRouterTargetConfigProtocol,
+
+#[derive(Derivative, Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(tag = "type", rename_all = "lowercase"))]
+pub enum OscRouterForwardTargetConfig {
+    #[cfg_attr(feature = "serde", serde(rename = "udp"))]
+    OscUdp(OscRouterForwardTargetOscUdpConfig),
+
+    #[cfg_attr(feature = "serde", serde(rename = "tcp"))]
+    OscTcp(OscRouterForwardTargetOscTcpConfig),
+
+    // todo: add WebSocket, etc.
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub struct OscRouterForwardConfig {
+    /// The target address to route the packet to.
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    target: OscRouterForwardTargetConfig,
 
     /// Optional address rewrite for the routed packet.
     rewrite: Option<String>,
@@ -40,7 +49,14 @@ pub struct OscRouterRouteConfig {
     /// Whether to stop propagating packets down the pipeline if matched.
     stop_propagation: bool,
 
-    route: Vec<OscRouterTargetConfig>,
+    #[cfg_attr(feature = "serde", serde(default))]
+    forward: Vec<OscRouterForwardConfig>,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub struct OscRouterConfig {
+    routes: Vec<OscRouterRouteConfig>,
 }
 
 #[cfg(test)]
@@ -49,25 +65,52 @@ mod tests {
 
     #[cfg(feature = "serde")]
     #[test]
-    fn test_deserialize_toml() {
-        let toml_str = r#"
-            [[routes]]
-            address = "/example/.*"
-            stop_propagation = true
+    fn test_deserialize_yaml() {
+        let yaml_str = r#"
+            routes:
+              - address: /avatar/parameters/(?<parameter>bHaptics_(?<position>.+)_(?<index>\d+)_bool)
+                stop_propagation: false
+                forward:
+                  - type: udp
+                    to: 127.0.0.1:23456 # target socket
+                    from: 127.0.0.1:23457 # source host socket for UDP
+                    rewrite: /$position/$index/bool
 
-            [[routes.route]]
-            to = "127.0.0.1:9000"
-            rewrite = "/new/address"
+                  - type: tcp
+                    to: 127.0.0.1:34567
+
+                  # - type: websocket
+                  #   to: ws://127.0.0.1:45678/osc
+
+              - address: /avatar/parameters/bHaptics_(?<parameter>.+)
+                stop_propagation: true
+
+              - address: /avatar/parameters/(?<parameter>.+)
+                stop_propagation: false
         "#;
 
-        let config: Vec<OscRouterRouteConfig> = toml::from_str(toml_str).unwrap();
+        let config: OscRouterConfig = serde_yaml::from_str(yaml_str).unwrap();
 
-        assert_eq!(config.len(), 1);
-        assert_eq!(config[0].address.as_str(), "/example/.*");
-        assert!(config[0].stop_propagation);
-        assert_eq!(config[0].route.len(), 1);
-        assert_eq!(config[0].route[0].to, "127.0.0.1:9000".parse().unwrap());
-        assert_eq!(config[0].route[0].protocol, OscRouterTargetConfigProtocol::OscUdp);
-        assert_eq!(config[0].route[0].rewrite.as_deref(), Some("/new/address"));
+        assert_eq!(config.routes.len(), 3);
+        assert_eq!(config.routes[0].address.as_str(), "/avatar/parameters/(?<parameter>bHaptics_(?<position>.+)_(?<index>\\d+)_bool)");
+        assert!(!config.routes[0].stop_propagation);
+        assert_eq!(config.routes[0].forward.len(), 2);
+        assert_eq!(config.routes[0].forward[0].target, OscRouterForwardTargetConfig::OscUdp(OscRouterForwardTargetOscUdpConfig {
+            to: "127.0.0.1:23456".parse().unwrap(),
+            from: "127.0.0.1:23457".parse().unwrap(),
+        }));
+        assert_eq!(config.routes[0].forward[0].rewrite.as_deref(), Some("/$position/$index/bool"));
+        assert_eq!(config.routes[0].forward[1].target, OscRouterForwardTargetConfig::OscTcp(OscRouterForwardTargetOscTcpConfig {
+            to: "127.0.0.1:34567".parse().unwrap(),
+        }));
+        assert!(config.routes[0].forward[1].rewrite.is_none());
+
+        assert_eq!(config.routes[1].address.as_str(), "/avatar/parameters/bHaptics_(?<parameter>.+)");
+        assert!(config.routes[1].stop_propagation);
+        assert_eq!(config.routes[1].forward.len(), 0);
+
+        assert_eq!(config.routes[2].address.as_str(), "/avatar/parameters/(?<parameter>.+)");
+        assert!(!config.routes[2].stop_propagation);
+        assert_eq!(config.routes[2].forward.len(), 0);
     }
 }
