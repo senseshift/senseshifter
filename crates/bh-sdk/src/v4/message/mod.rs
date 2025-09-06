6a67de43fb7cbbf9e9d6f507e4062f823d0ccc23
Leonid Meleshin <hello@leon0399.ru>
2025-09-06 13:41:36 +0200

    📝 (bH): document V4 SDK encryption flow

--- crates/bh-sdk/src/v4/message/mod.rs
+++ crates/bh-sdk/src/v4/message/mod.rs
@@ -1,3 +1,29 @@
+/// From observations, the `/v4/feedback` SDK uses the same messages as the `/v3/feedback`,
+/// but just wraps in an outer encryption layer.
+///
+/// The PKI is used to only safely exchange the client's AES-GCM key.
+/// All later messages are always encrypted using AES-GCM with the key above.
+///
+/// ## Message Flow
+///
+/// All the messages starting with the first SdkData are the same as in v3.
+///
+/// ```text
+///     ┌──────┐                                                                                                               ┌───┐
+//      │Server│                                                                                                               │SDK│
+//      └───┬──┘                                                                                                               └─┬─┘
+//          │                                  {"Type": "ServerKey", "Key": public_key_spki)}                                    │
+//          │───────────────────────────────────────────────────────────────────────────────────────────────────────────────────>│
+//          │                                                                                                                    │
+//          │              {"Type": "SdkClientKey", "Key": encrypt(PKCS#1 v1.5, public_key_spki, client_aes_key) }               │
+//          │<───────────────────────────────────────────────────────────────────────────────────────────────────────────────────│
+//          │                                                                                                                    │
+//          │{"Type": "SdkData", "Data": encrypt(AES-GSM, random_nonce[u8; 12], client_aes_key, "{\"type\": \"ServerReady\"}") } │
+//          │───────────────────────────────────────────────────────────────────────────────────────────────────────────────────>│
+//      ┌───┴──┐                                                                                                               ┌─┴─┐
+//      │Server│                                                                                                               │SDK│
+//      └──────┘
+/// ```
 use derivative::Derivative;
 use strum::{EnumDiscriminants, EnumString, IntoDiscriminant, VariantNames};
 
@@ -9,14 +35,22 @@ use strum::{EnumDiscriminants, EnumString, IntoDiscriminant, VariantNames};
 #[cfg_attr(feature = "serde", serde(tag = "Type"))]
 #[cfg_attr(feature = "serde", serde_with::serde_as)]
 pub enum SdkEncryptedMessage {
+  /// The server sends this message to the client to initiate the encryption handshake.
+  /// The key is usually a base64-encoded RSA 2048 public key (SPKI format).
   ServerKey {
     #[cfg_attr(feature = "serde", serde(rename = "Key"))]
     key: String,
   },
+
+  /// The client sends this message to the server to finish the encryption handshake.
+  /// The key is PKCS#1 v1.5-encrypted AES-GCM key, base64-encoded.
   SdkClientKey {
     #[cfg_attr(feature = "serde", serde(rename = "Key"))]
     key: String,
   },
+
+  /// Actual data from the SDK Client. Encrypted with the AES-GCM key above and base64-encoded.
+  /// Inside, it is a JSON string with the [crate::v3::SdkMessage] messages
   SdkData {
     #[cfg_attr(feature = "serde", serde(rename = "Data"))]
     data: String,
